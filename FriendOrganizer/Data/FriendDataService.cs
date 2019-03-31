c3d05f0c786543fc52e3fb3021fcf372a5ee9314
Radek Kurek <r.kurek@sputniksoftware.com>
2019-03-31 08:12:43 +0200

    Add project files.

---
+++ FriendOrganizer/Data/FriendDataService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FriendOrganizer.Model;
+
+namespace FriendOrganizer.Data
+{
+    public class FriendDataService
+    {
+
+        //TODO: Load from DB
+        public IEnumerable<Friend> GetAll()
+        {
+            yield return new Friend() { FirstName = "Jan", LastName = "Nowak", Email = "nowak@em.pl"};
+            yield return new Friend() { FirstName = "Anna", LastName = "Nowak", Email = "nowak2@em.pl" };
+            yield return new Friend() { FirstName = "Marek", LastName = "Kawałek", Email = "32234@em.pl" };
+            yield return new Friend() { FirstName = "Stefan", LastName = "Wolny", Email = "wlk@em.pl" };
+            yield return new Friend() { FirstName = "Marta", LastName = "Faworek", Email = "fww2@em.pl" };
+
+        }
+    }
+}
---
+++ FriendOrganizer/ViewModel/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FriendOrganizer.Model;
+
+namespace FriendOrganizer.ViewModel
+{
+    class MainViewModel
+    {
+        public MainViewModel()
+        {
+            
+        }
+
+        public void Load()
+        {
+
+        }
+
+        public ObservableCollection<Friend> Friends { get; set; }
+    }
+}
